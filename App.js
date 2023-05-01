f10ae81a191d15868d8eabf829c7f35fcb909549
StephenO'Donnell <stepheno@alchemytechs.com>
2023-05-01 22:45:17 +0100

    Some small changes to get the ball rolling Will need to look at some tutorials to better understand the dynamic of things

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <View style={styles.header}>
+        <Text>Testing the APP</Text>
+      </View>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
@@ -17,4 +21,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header: {
+    backgroundColor: 'aqua',
+    color: 'white',
+    width: '100%',
+    textAlign: 'center'
+  }
 });
---
+++ components/NavBar.js
@@ -0,0 +1,9 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function NavBar(){
+    return(
+        <View style={styles.NavBar}> 
+            <Text style={styles.NavBarText}></Text>
+        </View>
+    )
+}
